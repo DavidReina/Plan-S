06e7d8bc10a1f8bf9e6bc5e601a88bd6b5d5e2cf
carlosmorera1565 <carlos.morera@mail.escuelaing.edu.co>
2017-09-11 10:37:23 -0500

    controller

--- src/main/java/com/eci/cosw/springbootsecureapi/controller/UsuarioController.java
+++ src/main/java/com/eci/cosw/springbootsecureapi/controller/UsuarioController.java
@@ -1,4 +1,21 @@
 package com.eci.cosw.springbootsecureapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+
+@RestController
 public class UsuarioController {
-}
+
+    @RequestMapping(value = "/app/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public ResponseEntity<?> logout(Principal user) {
+        return new ResponseEntity<Object>(HttpStatus.ACCEPTED);
+    }
+    }
